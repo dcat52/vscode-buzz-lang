63632a1d00710d6b09aab93278a556ef84c05275
Davis <daviscatherman@gmail.com>
2023-01-16 11:09:22 -0500

    Sufficient development for soft release   - Significant keywork highlighting   - base language-configuration but handles comments correctly

--- test.py
+++ test.py
@@ -4,13 +4,30 @@ math.pi
 
 math.cos(5)
 
-if("hello")
+myD = {}
+
+myD[4] = "4"
+
+myD["hi"] = 3
+
+num = 5
+
+myD[num] = 5
+
+print(myD)
+
+
+if("hello"):
+    pass
 
 a = 5
 
+b = a*2+3/5//2**5
+
+my_tuple = (1, 2, 3.14)
+
 if(a == 4):
     print("hi" + 'hi')
-
 
 class myClass:
     def __init__(self, param):
@@ -24,5 +41,7 @@ class myClass:
         pass
 
 def myFx(param):
+    print(param)
     pass
 
+myFx(num)
